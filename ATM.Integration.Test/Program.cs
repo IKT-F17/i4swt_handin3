21a373daac664c035dabc9357db8ab7030e6028d
Tonni Lutze <tonni@witzell.dk>
2018-11-22 09:12:43 +0100

    Added initial Integration test solution and files to be able to setup integration for jenkins.

---
+++ ATM.Integration.Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using TransponderReceiver;
+
+namespace ATM.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var track = new TrackFactory(TransponderReceiverFactory.CreateTransponderDataReceiver());
+            new Airspace(track);
+            //new Telemetry(track);
+
+            Console.ReadKey();
+        }
+    }
+}

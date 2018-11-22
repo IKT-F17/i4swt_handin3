32f2cd6d746f4268d8f12e024e2661f3c6ec44d2
fred <f.predut@gmail.com>
2018-11-22 23:40:51 +0100

    Added unit test for TrackFactory

---
+++ ATM.Unit.Test/TrackFactoryUnitTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using ATM.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+using TransponderReceiver;
+
+namespace ATM.Unit.Test
+{
+    [TestFixture]
+    public class TrackFactoryUnitTest
+    {
+        private ITransponderReceiver _trFakeData;
+        private TrackFactory _uut;
+
+        
+        [SetUp]
+        public void Setup()
+        {
+            // Used in ReceiveCorrectDataFromTransponderReceiverDll:
+            _trFakeData = Substitute.For<ITransponderReceiver>();
+
+            // Used in SpawnTrackFromTrackFactory:
+            _uut = new TrackFactory(_trFakeData);
+        }
+
+        [Test]
+        public void ReceiveCorrectDataFromTransponderReceiverDll()
+        {
+            var testDataList = new List<string>
+            {
+                "PIE284;29388;49932;2000;20151006213456789",
+                "LIR511;46000;6800;8000;20171122211255510"
+            };
+
+            _trFakeData.TransponderDataReady += Raise.EventWith(this, new RawTransponderDataEventArgs(testDataList));
+        }
+
+    }
+}

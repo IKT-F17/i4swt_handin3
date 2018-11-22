4f8e555e753ad1d47951b0236831b3730d697ef8
Tonni Lutze <tonni@witzell.dk>
2018-11-22 23:44:40 +0100

    Test added to test SpawnTrack to ensure that it seperates the raw datastring correctly

--- ATM.Unit.Test/TrackFactoryUnitTest.cs
+++ ATM.Unit.Test/TrackFactoryUnitTest.cs
@@ -36,5 +36,19 @@ namespace ATM.Unit.Test
             _trFakeData.TransponderDataReady += Raise.EventWith(this, new RawTransponderDataEventArgs(testDataList));
         }
 
+        [Test]
+        public void SpawnTrackFromTrackFactory()
+        {
+            ITrack testTrack = _uut.SpawnTrack("PIE284;29388;49932;2000;20151006213456789");
+
+            Assert.That(testTrack.Tag, Is.EqualTo("PIE284"));
+            Assert.That(testTrack.XCoord, Is.EqualTo(29388));
+            Assert.That(testTrack.YCoord, Is.EqualTo(49932));
+            Assert.That(testTrack.Altitude, Is.EqualTo(2000));
+
+            var tesTime = new DateTime(2015, 10, 06, 21, 34, 56, 789);
+            Assert.That(testTrack.TimeStamp, Is.EqualTo(tesTime));
+        }
+
     }
 }
